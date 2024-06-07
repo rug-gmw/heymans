1ed5213c5cbc9ecbfd7980b824d9634caa5aa105
smathot <s.mathot@cogsci.nl>
2024-06-07 16:20:56 +0200

    Implement user endpoints - Dummy for now, to be replaced by LTS-compatible system later

--- heymans/routes/__init__.py
+++ heymans/routes/__init__.py
@@ -1,7 +1,6 @@
-from http import HTTPStatus
-from flask import jsonify, make_response
 from ._replies import no_content, not_found, success, forbidden, \
     invalid_json, missing_file, error
 from ._quizzes import quizzes_api_blueprint
 from ._app import app_blueprint
 from ._documents import documents_api_blueprint
+from ._user import user_api_blueprint
