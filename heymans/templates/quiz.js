d9d1af79111c1786656a7b014e58c490c1657fa9
Wouter <w.kruijne@gmail.com>
2024-06-14 16:05:31 +0200

    New Quiz uploads a son definition. Quizzes can get re-graded.

--- heymans/templates/quiz.js
+++ heymans/templates/quiz.js
@@ -3,8 +3,10 @@ const app = Vue.createApp({
     return {
       quizList: [],
       quizSelected: null,
+      quizName: '',
       gradingStatus: '',
       gradingResult: null,
+
 
       // isNewQuizCreated: false,
       // isGrading: false,
@@ -15,9 +17,11 @@ const app = Vue.createApp({
   },
   created() {
     this.fetchQuizList();
+    debugger;
   },
 
   methods: {
+    // gets the quiz list; also poll result from last quiz:
     async fetchQuizList() {
       const response = await fetch('/api/quizzes/list');
       this.quizList = await response.json();
@@ -25,50 +29,54 @@ const app = Vue.createApp({
       this.quizSelected = this.quizList.length ? this.quizList[this.quizList.length - 1].quiz_id : null;
       if (this.quizList.length) {
         this.pollGradingStatus(this.quizSelected);
+
       }
     },
 
+    // Get the grading status from the server:
     async pollGradingStatus(quiz_id) {
       this.quizSelected = quiz_id;
       const response = await fetch(`/api/quizzes/grading/poll/${quiz_id}`);
       this.gradingStatus = await response.json();
-      console.log(this.gradingStatus);
 
     if (this.gradingStatus["message"] === 'grading_done') {
         const quizResponse = await fetch(`/api/quizzes/get/${quiz_id}`);
         this.gradingResult = JSON.stringify(await quizResponse.json(), null, 2);
       } else if (this.gradingStatus['message'] == 'needs_grading') {
-      	console.log('needs_grading. Was the result deleted?')
         const quizResponse = await fetch(`/api/quizzes/get/${quiz_id}`);
         this.gradingResult = JSON.stringify(await quizResponse.json(), null, 2);
       } else { // grading_in_progress.
-      	console.log('grading_in_progress?; timeout')
         // setTimeout(this.pollGradingStatus, 1000);
 		setTimeout(() => this.pollGradingStatus(quiz_id), 1000);
       }
     },
 
-    // creating a new quiz (TODO: user data! (upload?))
-    async createNewQuiz() {
-      const quizData = {
-        name: 'Quiz title',
-        questions: [
-          {
-            text: 'Who is the cutest bunny?',
-            answer_key: '- Must state that the cutest bunny is Boef',
-            attempts: [
-              {
-                username: 's12345678',
-                answer: 'The cutest bunny is Boef',
-              },
-              {
-                username: 's12345678',
-                answer: "Don't know. :-(",
-              },
-            ],
-          },
-        ],
-      };
+    // When 'New quiz' gets clicked, the item is created
+    triggerFileUpload() {
+      document.getElementById('file-upload').click();
+    },
+
+    // After upload, the quiz gets parsed and the quiz gets populated..
+    handleQuizUpload(event) {
+      console.log("handling quiz upload ")
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          try {
+            const quizData = JSON.parse(e.target.result);
+            this.createNewQuiz(quizData);
+          } catch (error) {
+            console.error('Error parsing JSON file:', error);
+          }
+        };
+        reader.readAsText(file);
+      }
+    },
+
+    // creating a new quiz (by default, it's empty)
+    async createNewQuiz(quizData) {
+      console.log("Posting new quiz data:")
       const response = await fetch('/api/quizzes/new', {
         method: 'POST',
         headers: {
@@ -76,13 +84,17 @@ const app = Vue.createApp({
         },
         body: JSON.stringify(quizData),
       });
+      // TODO check whether response is valid. Give feedback.
+      // ...
+      // pull result, refresh the list
       const data = await response.json();
-      this.newQuizId = data.quizId;
-      this.isNewQuizCreated = true;
-      this.fetchQuizList();
+      await this.fetchQuizList();
+      // fetchQuizList already sets it to the last quiz
+      // new quiz is usually last quiz. This is probably superfluous?
+      this.quizSelected = data.quiz_id
     },
 
-    // Start grading a freshly created quiz; OR restart grading an ungraded quiz?
+    // Start grading OR restart grading:
     async startGrading() {
       this.isGrading = true;
       await fetch('/api/quizzes/grading/start', {
@@ -90,12 +102,17 @@ const app = Vue.createApp({
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({'quiz_id': this.newQuizId,
+        body: JSON.stringify({'quiz_id': this.quizSelected,
                               'model': 'mistral-medium'}),
     });
-      this.pollGradingStatus(this.newQuizId);
+      this.pollGradingStatus(this.quizSelected);
     },
   },
+  computed: {
+    isGrading() {
+      return this.gradingStatus.message === 'grading_in_progress';
+    }
+  }
 });
 
 app.mount('#app');
