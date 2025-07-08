5e934043db4ca5ccf4564a2e2e74e996989554d5
smathot <s.mathot@cogsci.nl>
2025-07-08 17:07:27 +0200

    Don't explicitly check if .env exists - dotenv already does that

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-import os
 from dotenv import load_dotenv
-# Check if .env exists in the folder of this file, and if so load it, otherwise
-# do nothing
-if os.path.exists(os.path.join(os.path.dirname(__file__), '.env')):
-    load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
-
+load_dotenv()
 from heymans.server import create_app
 from heymans import config
 
