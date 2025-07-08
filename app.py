b68130ce3d5849f5faa2b6da5f1c9bbd37a319fa
smathot <s.mathot@cogsci.nl>
2025-07-08 16:35:17 +0200

    Optionally load .env

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import os
+from dotenv import load_dotenv
+# Check if .env exists in the folder of this file, and if so load it, otherwise
+# do nothing
+if os.path.exists(os.path.join(os.path.dirname(__file__), '.env')):
+    load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
 from heymans.server import create_app
 from heymans import config
 
